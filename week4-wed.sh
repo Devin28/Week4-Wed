97ee1343a0289999d6af91cb2664d4fafc452e19
Devin-Brice <devin.brice@hgs.hiddengeniusproject.org>
2018-10-11 15:57:59 -0700

    first commit

---
+++ week4-wed.sh
@@ -0,0 +1,13 @@
+cd ~/desktop
+mkdir Week4-Wed
+cd Week4-Wed
+touch index.html
+mkdir css
+mkdir img
+cd css
+touch style1.css
+touch style2.css
+cd ../img
+curl -o img1.jpg "https://pm1.narvii.com/6282/a5b9c217e70a7927b54cc3dccb5f2bd764f5138a_hq.jpg"
+curl -o img2.jpg "https://pbs.twimg.com/media/BCTxK5xCcAEfUI2.jpg"
+curl -o img3.jpg "https://i.ytimg.com/vi/g3rxpf8j63I/hqdefault.jpg"
